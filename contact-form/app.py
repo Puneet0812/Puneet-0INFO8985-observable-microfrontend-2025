4ac46aa4f5d45f913b7792f7f52de83ae9402aa8
Richard Hildred <rhildred@gmail.com>
2025-04-10 19:22:22 +0000

    add fastapi server

---
+++ contact-form/app.py
@@ -0,0 +1,20 @@
+import uvicorn
+from fastapi import FastAPI, Request
+from fastapi import HTTPException, status
+from fastapi.staticfiles import StaticFiles
+from fastapi.middleware.cors import CORSMiddleware
+
+
+app = FastAPI()
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+app.mount('/', StaticFiles(directory="./dist", html=True), name="src")
+
+if __name__ == "__main__":
+    uvicorn.run(app, host="0.0.0.0", port=8001)
