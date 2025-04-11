09db38be37a343cd0b400c98059d428dd17dbd24
Richard Hildred <rhildred@gmail.com>
2025-04-11 14:29:30 +0000

    get a 202 response from sendgrid server with .env file

--- contact-form/app.py
+++ contact-form/app.py
@@ -3,6 +3,11 @@ from fastapi import FastAPI, Request
 from fastapi import HTTPException, status
 from fastapi.staticfiles import StaticFiles
 from fastapi.middleware.cors import CORSMiddleware
+import os
+from sendgrid import SendGridAPIClient
+from sendgrid.helpers.mail import Mail
+from dotenv import load_dotenv
+load_dotenv()
 
 
 app = FastAPI()
@@ -16,8 +21,19 @@ app.add_middleware(
 
 @app.post("/email")
 async def email(request: Request):
-    response = await request.json()
-    print(response)
+    messageContents = await request.json()
+    message = Mail(
+        to_emails=os.environ.get('SENDGRID_TO_EMAIL'),
+        from_email=os.environ.get('SENDGRID_FROM_EMAIL'),
+        subject=messageContents["subject"],
+        html_content= f"{messageContents["message"]}<br />{messageContents["name"]}")
+    message.reply_to = messageContents["email"]
+
+    try:
+        sg = SendGridAPIClient(os.environ.get('SENDGRID_API_KEY'))
+        response = sg.send(message)
+    except Exception as e:
+        return e
     return response
 
 app.mount('/', StaticFiles(directory="./dist", html=True), name="src")
